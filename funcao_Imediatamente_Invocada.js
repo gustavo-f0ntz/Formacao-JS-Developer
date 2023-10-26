f81f605c6e2dc13dcfd302fa7e89f6471a4c5747
Gustavo Kesley Fontes <72459288+gustavo-f0ntz@users.noreply.github.com>
2023-10-26 14:37:09 -0300

    Funcao imediatamente invocada

---
+++ funcao_Imediatamente_Invocada.js
@@ -0,0 +1,9 @@
+
+// utilizacao de funcao imediatamente invocada
+
+(function () {
+    console.log("Programa inicializando...");
+    console.log("Configurando padrões...");
+})();
+
+// isso é oara ser executada imediatamente, e isso isola escopo, é para quando o código for executado uma vez só.
---
+++ funcoes02.js
@@ -0,0 +1,17 @@
+
+
+// continuando com funcoes...
+
+
+
+function calcularJuros() {
+    console.log("Calculando Juros")
+}
+function main() {
+    console.log("Programa principal...")
+    calcularJuros();
+
+}
+
+main();
+
